23681fd2bdc23ae2364b3ccbf60e732d66fa79c5
liulisixin <yangyxwork@163.com>
2020-03-08 19:56:26 +0100

    task_d

--- week2/task_d_test.py
+++ week2/task_d_test.py
@@ -20,9 +20,9 @@ from detectron2.data import DatasetCatalog, MetadataCatalog
 import random
 
 def kitti_dataset(img_dir):
-    #dataset_location = '../KITTI/data_object_image_2/{}/image_2'.format(img_dir)
-    dataset_location = '../KITTI/data_object_image_2/{}'.format(img_dir)
-    gt_location = '../KITTI/training/label_2/'
+    #dataset_location = '../../KITTI/data_object_image_2/{}/image_2'.format(img_dir)
+    dataset_location = '../../KITTI/data_object_image_2/{}'.format(img_dir)
+    gt_location = '../../KITTI/training/label_2/'
     classes = {
         'Car' : 0, 
         'Van' : 1, 
@@ -34,10 +34,12 @@ def kitti_dataset(img_dir):
         'Misc' : 7,
         'DontCare' : 8
     }
-
+    
+    """
     gt_files = []
     for gt_file in os.listdir(gt_location):
         gt_files.append('{}{}'.format(gt_location, gt_file))
+    """
     # https://detectron2.readthedocs.io/tutorials/datasets.html
     dataset_dicts = []
 
@@ -52,6 +54,7 @@ def kitti_dataset(img_dir):
             record['width'] = width
             record['image_id'] = image_id
             
+            """
             gt_file = gt_files[image_id]
             objs = []
             with open(gt_file) as fh:
@@ -73,34 +76,20 @@ def kitti_dataset(img_dir):
                     }
                     objs.append(obj)
             record['annotations'] = objs
+            """
             dataset_dicts.append(record)
 
     return dataset_dicts
 
 
 if __name__ == "__main__":
-    for dataset_type in ['training']:
+    for dataset_type in ['testing/image_2']:
         DatasetCatalog.register("kitti_{}".format(dataset_type), lambda dataset_type = dataset_type : kitti_dataset(dataset_type))
         MetadataCatalog.get("kitti_{}".format(dataset_type)).set(
             thing_classes=['Car', 'Van', 'Truck', 'Pedestrian', 'Person_sitting',
                            'Cyclist', 'Tram', 'Misc', 'DontCare'])
-    kitti_metadata = MetadataCatalog.get("kitti_training")
+    kitti_metadata = MetadataCatalog.get("kitti_testing/image_2")
 
-    #try an example
-    dataset_type = 'training'
-    dataset_dicts = kitti_dataset(dataset_type)
-    for d in random.sample(dataset_dicts, 3):
-        img = cv2.imread(d["file_name"])
-        visualizer = Visualizer(img[:, :, ::-1], metadata=kitti_metadata, scale=0.5)
-        vis = visualizer.draw_dataset_dict(d)
-        # plt.imshow(vis.get_image()[:, :, ::-1])
-        # cv2.imshow('image', vis.get_image()[:, :, ::-1])
-
-        cv2.imwrite('test.png', vis.get_image()[:, :, ::-1])
-
-
-    # model = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
-    # model = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
     model = "COCO-Detection/faster_rcnn_R_50_C4_1x.yaml"
 
     cfg = get_cfg()
@@ -111,12 +100,29 @@ if __name__ == "__main__":
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model)  # Let training initialize from model zoo
     cfg.SOLVER.IMS_PER_BATCH = 1
     cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32   # faster, and good enough for this toy dataset (default: 512)
+    cfg.SOLVER.MAX_ITER = 300  # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32  # faster, and good enough for this toy dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 9  # only has one class (ballon)
 
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-    trainer = DefaultTrainer(cfg)
 
-    trainer.resume_or_load(resume=False)
-    trainer.train()
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set the testing threshold for this model
+    cfg.DATASETS.TEST = ("kitti_val",)
+    predictor = DefaultPredictor(cfg)
+
+    from detectron2.utils.visualizer import ColorMode
+
+    dataset_dicts = kitti_dataset("testing/image_2")
+    for d in random.sample(dataset_dicts, 3):
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)
+        v = Visualizer(im[:, :, ::-1],
+                       metadata=kitti_metadata,
+                       scale=0.8,
+                       # instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
+                       )
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        cv2.imwrite('evaluation{}.png'.format(d["file_name"].split("/")[-1]), v.get_image()[:, :, ::-1])
+
+        
